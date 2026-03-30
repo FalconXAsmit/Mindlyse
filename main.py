d0c65e00a38073d0cd64003f7563d8666d80dc04
Asmit Kumar <kumarasmit2007@gmail.com>
2026-03-30 16:09:53 +0530

    File upload endpoint working

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message" : "Mindlyse is alive"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    contents = await file.read()
+    return{
+        "filename": file.filename,
+        "size_bytes": len(contents),
+        "content_type": file.content_type
+    }
